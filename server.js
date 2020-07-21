4f7dbd4d6ff8d5a7ff47384992146d3f50f5fc87
Madeleine <madeleinemin@uchicago.edu>
2020-07-20 20:40:20 -0500

    Changed default port

--- server.js
+++ server.js
@@ -8,6 +8,5 @@ app.get('/', (req, res) => {
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
 
-const server = app.listen(7000, () => {
-  console.log(`Express running → PORT ${server.address().port}`);
-});
+
+const server = app.listen(process.env.PORT || 5000);
